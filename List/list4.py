21a447e798930ff3941776c4ce344f018bc7465e
v2jarvis <bittoojarvis@gmail.com>
2023-09-01 02:50:55 +0530

    string palindrome using list and slicing

---
+++ List/list4.py
@@ -0,0 +1,9 @@
+# create a list which one have multiple elements like string, integer, boolean etc
+l1=["xyz",23,[33,True,32,[40,41,42,43,[7,8,9],44,45],30,29,28],24,25,26,27]
+
+#use slice using : symbol
+#print(l1[:])
+
+print(l1[::-1])
+
+
---
+++ List/string_palindrome.py
@@ -0,0 +1,7 @@
+l1=list(input("Enter The String:-"))
+
+l2=l1[::-1]
+if(l1==l2):
+    print("String is Palindrome")
+else:
+    print("String is not Palindrome")
