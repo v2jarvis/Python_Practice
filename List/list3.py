d408f91d54da863bdf6fb8dbbadd6228c4e9048a
v2jarvis <bittoojarvis@gmail.com>
2023-09-01 02:39:06 +0530

    list3 updated positive and negative indexing use

---
+++ List/list3.py
@@ -0,0 +1,10 @@
+# create a list which one have multiple elements like string, integer, boolean etc
+l3=["xyz",23,[33,True,32,[40,41,42,43,[7,8,9],44,45],30,29,28],24,25,26,27]
+
+#get value using negative index
+print(l3[-5][-4][-3][-2])
+
+#get value using positive index
+print(l3[2][3][4][1])
+
+#output = 8 in both case
