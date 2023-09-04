cebaf54a46b2053d0bc0ef95b993d7ed3e4660e7
v2jarvis <bittoojarvis@gmail.com>
2023-09-05 00:04:13 +0530

    remove_duplicate_elements_using_second_way

--- List/list.py
+++ List/list.py
@@ -1,11 +1,17 @@
 # remove duplicate elements from the list
-#l1=input("Enter The List Elements:-").split(',')
+l1=input("Enter The List Elements:-").split(',')
 
-ele=input("Enter The Elements:-")
+'''ele=input("Enter The Elements:-")
 l1 = [int(x) for x in ele.split(',')]
 l2=[]
 for i in l1:
     if i not in l2:
         l2.append(i)
-print(l2)
+print(l2)'''
 
+#second method
+for i in l1:
+    if(l1.count(i)>1):
+        for j in range(l1.count(i)-1):
+            l1.remove(i)
+print(l1)
