a9e68bf9fab3996d918eeb1257877b8e35499784
v2jarvis <bittoojarvis@gmail.com>
2023-09-05 00:21:19 +0530

    add_two_list

---
+++ List/list5.py
@@ -0,0 +1,9 @@
+'''list1=list(input("Enter The List Elements:-"))
+list2=list(input("Enter The List Elements:-"))
+
+list3=[]
+for i,j in zip(list1,list2):
+    output=i+j
+    list3.append(output)
+print(list3)'''
+
