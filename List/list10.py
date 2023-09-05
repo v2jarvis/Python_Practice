bc99426b859cffeb7796f0368649cda8374bfbe1
v2jarvis <bittoojarvis@gmail.com>
2023-09-06 01:16:06 +0530

    Min to number

---
+++ List/list10.py
@@ -0,0 +1,7 @@
+# Minimum of two numbers in Python
+num1=int(input("Enter The First Number:-"))
+num2=int(input("Enter The Second Number:-"))
+if(num1>num2):
+    print(num2)
+else:
+    print(num1)
---
+++ List/list9.py
@@ -0,0 +1,5 @@
+#remove nested list from the list
+l1=input("Enter the list and Nested List:-").split(',')
+#l1=[int(item) for item in l1]
+print(l1)
+print(type(l1))
