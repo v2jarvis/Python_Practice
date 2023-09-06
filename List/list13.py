70b36de93858d43db77e1ecceb6c9541510a5cb1
v2jarvis <bittoojarvis@gmail.com>
2023-09-07 01:10:42 +0530

    two pointer apporch

--- List/list13.py
+++ List/list13.py
@@ -1,11 +1,20 @@
 #reverse a list
-l1=[1,2,3,4,5,6,7,8,9]
+#l1=[1,2,3,4,5,6,7,8,9]
 #print(l1[::-1])
 
 #second way
-l1.reverse()
-print(l1)
+#l1.reverse()
+#print(l1)
 
-#third way
-#for i in range(len(l1)-1):
-#    print(i)
+#third way two pointer apporch
+l1=[1,2,3,4,5,6,7,8,9]
+l=0
+r=len(l1)-1
+
+while(l<r):
+    temp=l1[l]
+    l1[l]=l1[r]
+    l1[r]=temp
+    l=l+1
+    r=r-1
+print(l1)
