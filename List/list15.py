d6400b9d9bd8139b463b126b9370075ac486bd33
v2jarvis <bittoojarvis@gmail.com>
2023-09-08 00:19:58 +0530

    Remove empty List from List using second method

--- List/list15.py
+++ List/list15.py
@@ -1,6 +1,15 @@
 #Remove empty List from List
-l1=[1,2,3,4,5,[4,5,5],6,7,8,9,[455],10,11]
-for i in l1:
+#first and without any predefine function use
+l1=[1,2,3,4,5,[],6,7,8,9,[],10,11]
+
+'''for i in l1:
     if(type(i)==list and len(i)>0):
         l1.remove(i)
+print(l1)'''
+
+#second method
+while [] in l1:
+    l1.remove([])
 print(l1)
+
+
