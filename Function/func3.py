611dc7c8e4ff301c843dbc70e439c78a97ceb1ba
v2jarvis <bittoojarvis@gmail.com>
2023-09-15 01:28:07 +0530

    add two number using function

---
+++ Function/func3.py
@@ -0,0 +1,9 @@
+#add two number using function
+def add(num1:int,num2:int):
+    val=num1+num2
+    return val
+num1=int(input("Enter The First Number:-"))
+num2=int(input("Enter The Second Number:-"))
+
+result=add(num1,num2)
+print(result)
