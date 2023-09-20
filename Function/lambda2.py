5523de7fb27c6a26aa4211fdff1567cf17d51a2f
v2jarvis <bittoojarvis@gmail.com>
2023-09-21 00:01:45 +0530

    lambda func second example take one arg and double it

---
+++ Function/lambda2.py
@@ -0,0 +1,4 @@
+#takes one argument, and that argument will be multiplied with an unknown given number.
+fun=lambda x:x*2
+val=int(input("Enter The Number Which Are You Want to Double:-"))
+print(fun(val))
