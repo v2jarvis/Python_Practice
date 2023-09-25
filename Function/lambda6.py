cd23226ae2aea657b0c0fe3f8d7af3d383ee093a
v2jarvis <bittoojarvis@gmail.com>
2023-09-25 23:59:30 +0530

    larg of sublist

---
+++ Function/lambda6.py
@@ -0,0 +1,7 @@
+my_List = [ [3, 5, 8, 6], [23, 54, 12, 87], [1, 2, 4, 12, 5] ]
+# sorting every sublist of the above list
+sort_List = lambda num : ( sorted(n) for n in num )
+# Getting the third largest number of the sublist
+third_Largest = lambda num, func : [ l[ len(l) - 2] for l in func(num)]
+result = third_Largest( my_List, sort_List)
+print('The third largest number from every sub list is:', result )
