a8801ff3cb865e428ff06c35b27b42172b9671eb
v2jarvis <bittoojarvis@gmail.com>
2023-10-03 01:06:52 +0530

    second minimum

--- List/list22.py
+++ List/list22.py
@@ -1,10 +1,9 @@
-#find the second minimum elements without using any function or method
+#find the second minimum elements without using any function or method.
 
 #l1=input("Enter The List Elements:").split()
 l1=[2,8,3,1,7,4,6,9,3,5]
 max=l1[0]
 max1=l1[0]
-max2=l1[0]
 
 for i in l1:
     if (i>max):
@@ -12,8 +11,5 @@ for i in l1:
         max=i
     elif(i>max1 and i!=max):
         max1=i
-    elif(i>max2 and i!=max):
-        max2=i
-    if(max2!=max):
-        pass
-print(max)
+    if(max1==max):
+        print(max)
