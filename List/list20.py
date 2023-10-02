247bf5ec389adb49973d381846043e4262a1ed19
v2jarvis <bittoojarvis@gmail.com>
2023-10-02 19:35:51 +0530

    find the minimum elements

---
+++ List/list20.py
@@ -0,0 +1,13 @@
+#find the minimum elements betwout using any function or method
+l1=[2,8,3,1,7,4,6,9,3,5]
+
+#using method
+print(min(l1))
+
+n=len(l1)
+mx=l1[0]
+
+for i in range(1,n):
+    if (l1[i]<mx):
+        mx=l1[i]
+print(mx)
