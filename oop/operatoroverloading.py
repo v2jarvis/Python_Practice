cda6b3b4e0641122f1acaa89abb7ec41adf61a5d
v2jarvis <bittoojarvis@gmail.com>
2023-10-06 00:21:13 +0530

    simple example of operator overloading

--- oop/operatoroverloading.py
+++ oop/operatoroverloading.py
@@ -2,12 +2,18 @@
 class cal:
     def __init__(self,a) -> None:
         self.a=a
-    def __mul__(t1,t2):
-        return (t1.a*t2.a)
     def __add__(t1,t2):
         return(t1.a+t2.a)
+    def __sub__(t1,t2):
+        return (t1.a-t2.a)
+    def __mul__(t1,t2):
+        return (t1.a*t2.a)
+    def __truediv__(t1,t2):
+        return (t1.a/t1.a)
     
-t1=cal(5)
-t2=cal(10)
+t1=cal(int(input("Enter the fisrt number for add,sub,mul and div:")))
+t2=cal(int(input("Enter the second number for add,sub,mul and div:")))
+print(t1+t2)
+print(t1-t2)
 print(t2*t1)
-print(t1+t2)        
+print(t1/t2)
