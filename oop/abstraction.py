ee02ca4f8afbeab3a7aed848a84cbb6dad560a2f
v2jarvis <bittoojarvis@gmail.com>
2023-10-08 23:52:54 +0530

    create simple calculator using of abstraction

--- oop/abstraction.py
+++ oop/abstraction.py
@@ -4,9 +4,14 @@ class test(ABC):
     @abstractmethod
     def engine(self,a,b):
         pass
-class calc(test):
+class add(test):
     def engine(self,a,b):
         return a+b
+class sub(test):
+    def engine(self,a,b):
+        return a-b
 
-obj=calc()
+obj=add()
 print(obj.engine(32,23))
+obj1=sub()
+print(obj1.engine(32,23))
