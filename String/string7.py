a9dabdf2fb3d359fd7a8a07e1da48709632c4be5
v2jarvis <bittoojarvis@gmail.com>
2023-10-20 00:49:54 +0530

    perform multiple task on the string-->>

--- String/string7.py
+++ String/string7.py
@@ -1,8 +1,17 @@
 #perform multiple task on the string-->>
 
-s1="how are you, I am fine, tellme about you?"
+
+'''s1="Niteesh"
+s2="hello how are you"
+s2=s2.replace(" ","") #The replace() method replaces a specified phrase with another specified phrase.
+print(s2)
+s1=s1.replace("Niteesh","v2jarvis")
+print(s1)'''
+
+'''s1="how are you, I am fine, tellme about you?"
 s1=s1.split(' ') #split use to seprate the value and convert a string into a list
-print(s1)
+print(s1)'''
+
 '''s1='v2jarvis'
 s1=s1.split() #The split() method splits a string into a list. You can specify the separator, default separator is any whitespace.
 print(s1)'''
