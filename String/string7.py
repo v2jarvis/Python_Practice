a6f1246c9482eaca398ea2c6f705602553c00eee
v2jarvis <bittoojarvis@gmail.com>
2023-10-20 00:35:05 +0530

    perform multiple task on the string-->>

---
+++ String/string7.py
@@ -0,0 +1,18 @@
+#perform multiple task on the string-->>
+
+s1="how are you, I am fine, tellme about you?"
+s1=s1.split(' ') #split use to seprate the value and convert a string into a list
+print(s1)
+'''s1='v2jarvis'
+s1=s1.split() #The split() method splits a string into a list. You can specify the separator, default separator is any whitespace.
+print(s1)'''
+
+'''s1='Niteesh'
+s1=''.join(reversed(s1)) # The join() method takes all items in an iterable and joins them into one string.
+#s1.count()
+print(s1)'''
+
+'''s1=('Niteesh','Kumar','Yadav')
+s1=''.join(s1)
+print(s1)'''
+
