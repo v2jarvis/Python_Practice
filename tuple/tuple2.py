c8d73873c96736561cd9973ee593544ceb855819
v2jarvis <bittoojarvis@gmail.com>
2023-10-28 00:10:30 +0530

    perform multiple task on tuple

--- tuple/tuple2.py
+++ tuple/tuple2.py
@@ -1,4 +1,11 @@
+#perform multiple task on tuple
+t1=(10,20,20,23,5,2,90)
+#print(t1.count(20)) # for count the value
+print(t1.index(20)) #give the value and show the index of it
+
 #find the largest elements in a tuple
-t1=(10,20,23,5,2,90)
-t2=list(t1)
-print(max(t2))
+#t2=list(t1)
+#print(max(t2))
+
+
+
