8167b98b20426633d71750471a2ee8a1294ed669
v2jarvis <bittoojarvis@gmail.com>
2023-11-04 01:35:30 +0530

    simple perform the task 4 occurance in the list

--- List/list24.py
+++ List/list24.py
@@ -1,6 +1,25 @@
 #simple perform the task 4 occurance in the list
-l1=[1,4,[4,3,4,5],[4,5,5,4,4]]
+'''l1=[1,4,[4,3,4,5],[4,5,5,4,4]]
 for i in l1:
     if type(i)==list:
-        y=(l1.count(4))
-        print(y)
+        y=(l1.count(4)-1)
+print(y)'''
+l1 = [1, 4, [4, 3, 4, 5], [4, 5, 5, 4, 4]]
+total_count = 0
+skip_first = True
+
+for item in l1:
+    if type(item) is list:
+        for sub_item in item:
+            if sub_item == 4:
+                if skip_first:
+                    skip_first = False
+                else:
+                    total_count += 1
+    elif item == 4:
+        if skip_first:
+            skip_first = False
+        else:
+            total_count += 1
+
+print("Total occurrences of 4 (after skipping the first one):", total_count)
---
+++ oop/classvar.py
@@ -0,0 +1,16 @@
+#creating the class var and perform the multiple task on it
+class MyClass:
+    class_var = 0  # Class variable
+
+    def __init__(self, value):
+        self.instance_var = value
+
+obj1 = MyClass(10)
+obj2 = MyClass(20)
+
+print(obj1.class_var)  # Output: 0
+print(obj2.class_var)  # Output: 0
+
+MyClass.class_var = 5  # Change the class variable
+print(obj1.class_var)  # Output: 5
+print(obj2.class_var)  # Output: 5
