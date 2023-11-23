90457af59c5833cf61ac2c317ffbc1cac57a367b
v2jarvis <bittoojarvis@gmail.com>
2023-11-23 23:39:12 +0530

    class method practice and use the class var

--- oop/Class Method/class1.py
+++ oop/Class Method/class1.py
@@ -1,7 +1,11 @@
 #class method practice
 class Test:
+    salary=20000 #class varriable same for every one
     def __init__(self,name,address):
         self.n=name
         self.a=address
 t1=Test('Niteesh','Noida')
-print(t1.n,t1.a)
+t1.salary=25000
+print(t1.n,t1.a,t1.salary)
+t2=Test('Uday','Noida')
+print(t2.n,t2.a,t2.salary)
