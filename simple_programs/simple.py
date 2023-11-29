ec896af82cb8371cac7c90177b92cab73293ad1a
v2jarvis <bittoojarvis@gmail.com>
2023-11-30 00:20:38 +0530

    perform multiple task on list

--- simple_programs/simple.py
+++ simple_programs/simple.py
@@ -1,5 +1,11 @@
 #perform multiple task on list
-a=[1,2,3,4,5,6]
-val=a.pop()
+'''a=[1,2,3,4,5,6]
+val=a.pop() # pop with no argument perform the remove element by last side
 print(val)
-print(a)
+print(a)'''
+'''a=[1,2,3,4,5,6]
+val=a.pop(2) #pop arugument take index to remove the element
+print(val)
+print(a)'''
+
+a=[1,2,3,4,5,6]
