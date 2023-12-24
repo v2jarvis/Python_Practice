8fb9b77e4cb5b226803f2f54000f7de87dcfa1ce
v2jarvis <bittoojarvis@gmail.com>
2023-12-24 20:38:28 +0530

    perform multiple method on set

---
+++ set/set4.py
@@ -0,0 +1,11 @@
+#perform multiple method on set
+s1={1,2,3,4,5,6,7,8,9}
+s2={1,2,3,10,11}
+s1.add(10) #add element in the set
+print(s1)
+s1.pop() #pop remove item from anywhere
+print(s1)
+s1.remove(2) #remove the given element
+print(s1)
+s1.update(s2)
+print(s1)
