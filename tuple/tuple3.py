2b9b2d15c3af06a137a65412029b8f46be8860df
v2jarvis <bittoojarvis@gmail.com>
2023-12-24 20:53:44 +0530

    perfrom multiple method on tuple

---
+++ tuple/tuple3.py
@@ -0,0 +1,6 @@
+#perfrom multiple method on tuple
+t=(1,2,3,4,5,6,7,8,9,8,8,7,9)
+cn=t.count(8) #count the occurance of element in the tuple
+ind=t.index(4) #give the element and give the index
+print(cn)
+print(ind)
