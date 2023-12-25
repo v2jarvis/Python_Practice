16ea2799201f7eae6fcde6b5328470406f4d2a5d
v2jarvis <bittoojarvis@gmail.com>
2023-12-26 00:01:53 +0530

    everse a string and the list without using any function or method

--- String/string10.py
+++ String/string10.py
@@ -1,6 +1,7 @@
-#reverse a string without using any function or method
-s="niteesh"
-s2=''
+#reverse a string and the list without using any function or method
+#s="niteesh"
+s=[1,2,3,4,5,6,7,8,9,10]
+s2=[]
 for i in range(len(s)-1,-1,-1):
-    s2+=s[i]
+    s2.append(s[i])
 print(s2)
