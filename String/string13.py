87c4e7eabe1d70dc816ee2f82c3747e3fee8330d
v2jarvis <bittoojarvis@gmail.com>
2024-02-12 23:30:47 +0530

    reverse vowels of a string

---
+++ String/string13.py
@@ -0,0 +1,16 @@
+#reverse vowels of a string
+s1='hello'
+s2=''
+vowels='aeiouAEIOU'
+l1=[]
+
+for i in s1:
+    if i in vowels:
+        l1.append(i)
+
+for j in s1:
+    if j in vowels:
+        s2+=l1.pop()
+    else:
+        s2+=j
+print(s2)
