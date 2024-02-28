8740d90ea65e2a9ed49cdd688e3891584c057a18
v2jarvis <bittoojarvis@gmail.com>
2024-02-29 01:45:34 +0530

    add the three digits and above three digit multiply

--- List/list39.py
+++ List/list39.py
@@ -1,7 +1,26 @@
 #add the three digits and above three digit multiply
-l1=[100,1002,203,3002]
+l1=[100,1112,243,3142]
 for i in l1:
     if i>1000:
+        mul=1
+        temp=i
+        while(temp>0):
+            rem=temp%10
+            mul=mul*rem
+            temp=temp//10
+        print(mul)
+    else:
+        add=0
+        temp=i
+        while(temp>0):
+            rem=temp%10
+            add=add+rem
+            temp=temp//10
+        print(add)
+
+
+
+    '''if i>1000:
         temp=0
         rem=i%10
         val=i-rem
@@ -14,5 +33,5 @@ for i in l1:
         val=i-rem
         ele=val/100
         temp=rem+ele
-        print(int(temp))
+        print(int(temp))'''
 
