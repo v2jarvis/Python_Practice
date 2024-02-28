533fcea0b27abac346be39960b99e13b76a85d83
v2jarvis <bittoojarvis@gmail.com>
2024-02-29 01:49:34 +0530

    add the value less then 50 and above multiply in the given list

--- List/list40.py
+++ List/list40.py
@@ -1,8 +1,25 @@
 #add the value less then 50 and above multiply in the given list
-l1=[41,42,49,52,62]
-
+l1=[41,42,49,52,624]
 for i in l1:
     if i<50:
+        add=0
+        temp=i
+        while (temp>0):
+            rem=temp%10
+            add=add+rem
+            temp=temp//10
+        print(add)
+    else:
+        mul=1
+        temp=i
+        while(temp>0):
+            rem=temp%10
+            mul=mul*rem
+            temp=temp//10
+        print(mul)
+
+
+    '''if i<50:
         temp=0
         data=i%10
 #        print(data)
@@ -16,7 +33,7 @@ for i in l1:
         val=i-data
         ele=val/10
         temp=data*ele
-        print(int(temp))
+        print(int(temp))'''
 
 
 #wrong
