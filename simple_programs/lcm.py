4fdcae4c5c77391acb71d9334ae6425a1d436190
v2jarvis <bittoojarvis@gmail.com>
2024-03-31 01:20:51 +0530

    simple lcm program

--- simple_programs/lcm.py
+++ simple_programs/lcm.py
@@ -1,5 +1,67 @@
 #simple lcm program
-a=75
+l1=[2,5,8]
+temp=0
+
+if l1[0]>l1[1]:
+    temp=l1[0]
+elif(l1[1]>l1[2]):
+    temp=l1[0]
+elif(l1[2]>l1[1]):
+    temp=l1[2]
+#else:
+#    temp=l1[3]
+
+count=0
+while(True):
+       if (temp%l1[0]==0 and temp%l1[1]==0 and temp%l1[2]==0):
+           val=temp
+           break
+       temp+=1
+print(val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''a=75
 b=45
 temp=0
 count=0
@@ -13,4 +75,4 @@ while(True):
            val=temp
            break
        temp+=1
-print(val)
+print(val)'''
