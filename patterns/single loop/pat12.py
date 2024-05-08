47364e1282ad7ddc7439040248fd56a07ff6c021
v2jarvis <bittoojarvis@gmail.com>
2024-05-09 00:57:08 +0530

    pattern practice

---
+++ patterns/single loop/pat12.py
@@ -0,0 +1,18 @@
+'''
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+'''
+
+num=int(input("Enter the len:"))
+for i in range(2*num+1):
+    if num>i:
+        print(" "*(num-i)+"* "*i)
+    else:
+        print(" "*(i-num)+"* "*(2*num-i))
