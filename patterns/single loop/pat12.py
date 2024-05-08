f0e1b2e7734ef80909652d6ab88a4cb5979c69ab
v2jarvis <bittoojarvis@gmail.com>
2024-05-09 00:59:46 +0530

    pattern practice

--- patterns/single loop/pat12.py
+++ patterns/single loop/pat12.py
@@ -16,3 +16,8 @@ for i in range(2*num+1):
         print(" "*(num-i)+"* "*i)
     else:
         print(" "*(i-num)+"* "*(2*num-i))
+
+
+    # space=abs(num-i)
+    # stars=num-space
+    # print(" "*space+" *"*stars)
